c955a74fc5ee8ccd9626e3f3be0ada63cac18623
Luca Anthony <123425103+antth-Luca@users.noreply.github.com>
2026-06-08 17:50:31 -0300

    ...

--- neoforge-project/Brutore/src/main/java/io/github/antthluca/brutore/init/InitBlockEntities.java
+++ neoforge-project/Brutore/src/main/java/io/github/antthluca/brutore/init/InitBlockEntities.java
@@ -1,12 +1,12 @@
 package io.github.antthluca.brutore.init;
 
+import java.util.function.Supplier;
+
 import io.github.antthluca.brutore.Brutore;
 import io.github.antthluca.brutore.blocks.entity.RawInatorBlockEntity;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.neoforged.neoforge.registries.DeferredRegister;
-
-import java.util.function.Supplier;
 
 public class InitBlockEntities {
     public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES = DeferredRegister.create(
@@ -16,6 +16,5 @@ public class InitBlockEntities {
     public static final Supplier<BlockEntityType<RawInatorBlockEntity>> RAW_INATOR_BE = BLOCK_ENTITIES.register(
             "raw_inator_be", () -> BlockEntityType.Builder.of(
                     RawInatorBlockEntity::new,
-                    InitBlocks.RAW_INATOR.get()
-            ).build(null));
+                    InitBlocks.RAW_INATOR.get()).build(null));
 }
